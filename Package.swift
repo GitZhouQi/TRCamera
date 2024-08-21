e866a5e8db094b5d2fe07e8af1931de42a2e7663
GitIos <478930170@qq.com>
2024-08-21 14:13:25 +0800

    init

--- Package.swift
+++ Package.swift
@@ -4,17 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "MijickCameraView",
+    name: "TRCameraView",
     platforms: [
         .iOS(.v14)
     ],
     products: [
-        .library(name: "MijickCameraView", targets: ["MijickCameraView"]),
+        .library(name: "TRCameraView", targets: ["TRCameraView"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Mijick/Timer", from: "1.0.1")
     ],
     targets: [
-        .target(name: "MijickCameraView", dependencies: [.product(name: "MijickTimer", package: "Timer")], path: "Sources")
+        .target(name: "TRCameraView", dependencies: [.product(name: "MijickTimer", package: "Timer")], path: "Sources")
     ]
 )
